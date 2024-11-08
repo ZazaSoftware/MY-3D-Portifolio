2ef4910f962398420905ce2d7205a9903dc97ad3
ZazaSoftware <zazasoftwaree@gmail.com>
2024-11-08 00:26:05 -0800

    Update About.jsx

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -25,7 +25,7 @@ const About = () => {
             <div>
               <p className="grid-headtext">Hi, I’m Zaza Software</p>
               <p className="grid-subtext">
-                With 12 years of experience, I have honed my skills in both frontend and backend dev, creating dynamic
+                With 5 years of experience, I have honed my skills in both frontend and backend dev, creating dynamic
                 and responsive websites
               </p>
             </div>
